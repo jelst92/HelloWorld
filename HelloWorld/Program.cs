a29ef20a30fd60a60fa499c5bfb816da670d96f1
jelst92 <joseph.parker92@outlook.com>
2023-05-02 12:11:40 +0100

    branch 2?

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Hello \n World!");
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            string day;
+            switch (num)
+            { case 1: day = "Monday"; break;
+                case 2: day = "Tuesday"; break;
+                case 3: day = "Wednesday"; break;
+                case 4: day = "Thursday"; break;
+                case 5: day = "Friday"; break;
+                default: day = "Weekend Day"; break; 
+            }
+                Console.WriteLine("Day" + num + ":" + day);
+            Console.ReadKey();
+                                 }
     }
 }
